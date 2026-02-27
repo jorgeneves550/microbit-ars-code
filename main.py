31dceba2ed94c949f4286d1239314a242969dfea
Jorge Silva <jorgeneves550@zarco.pt>
2026-02-27 16:23:39 +0000

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+radio.set_group(1)
+coluna = 0
+linha = 0
+alunoLed = 0
+numAleatorio = 0
+totalAlunos = 10
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
-})
+let numAleatorio = 0
+let alunoLed = 0
+let linha = 0
+let coluna = 0
+radio.setGroup(1)
+let totalAlunos = 10
