40756035ed9dcc05a3ebdf876c0bb39cd996a950
Jorge Silva <jorgeneves550@zarco.pt>
2026-02-27 18:16:03 +0000

    "Função para acionar o micro:bit do aluno em função do valor recebido

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-handrise = 0
-radio.set_group(1)
-aluno = 1
-
+def notifica(texto: str, num2: number):
+    if texto == "ambAula":
+        if num2 == 1:
+            for index in range(4):
+                basic.show_icon(IconNames.NO)
+                basic.clear_screen()
+                music.play(music.tone_playable(330, music.beat(BeatFraction.QUARTER)),
+                    music.PlaybackMode.UNTIL_DONE)
+        else:
+            for index2 in range(4):
+                basic.show_icon(IconNames.YES)
+                basic.clear_screen()
+    elif texto == "alunoAlt":
+        if num2 == aluno:
+            for index3 in range(4):
+                basic.show_leds("""
+                    . . # . .
+                    . # # # .
+                    # # # # #
+                    . # # # .
+                    . . # . .
+                    """)
+                music.play(music.tone_playable(784, music.beat(BeatFraction.EIGHTH)),
+                    music.PlaybackMode.UNTIL_DONE)
+                basic.show_leds("""
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    """)
 def intervir(num: number):
+    handrise = 0
     if handrise == 0:
         basic.show_icon(IconNames.HAPPY)
     else:
         basic.show_icon(IconNames.CONFUSED)
+aluno = 0
+radio.set_group(1)
+aluno = 1
--- main.ts
+++ main.ts
@@ -1,10 +1,53 @@
-function intervir (num: number) {
+function notifica(texto: string, num2: number) {
+    if (texto == "ambAula") {
+        if (num2 == 1) {
+            for (let index = 0; index < 4; index++) {
+                basic.showIcon(IconNames.No)
+                basic.clearScreen()
+                music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+            }
+        } else {
+            for (let index2 = 0; index2 < 4; index2++) {
+                basic.showIcon(IconNames.Yes)
+                basic.clearScreen()
+            }
+        }
+        
+    } else if (texto == "alunoAlt") {
+        if (num2 == aluno) {
+            for (let index3 = 0; index3 < 4; index3++) {
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # # # # #
+                    . # # # .
+                    . . # . .
+                    `)
+                music.play(music.tonePlayable(784, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    `)
+            }
+        }
+        
+    }
+    
+}
+
+function intervir(num: number) {
     let handrise = 0
     if (handrise == 0) {
         basic.showIcon(IconNames.Happy)
     } else {
         basic.showIcon(IconNames.Confused)
     }
+    
 }
+
+let aluno = 0
 radio.setGroup(1)
-let aluno = 1
+aluno = 1
