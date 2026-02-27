178c7ed44ffa7e51f3690738246ad3dd46cf3a31
Jorge Silva <jorgeneves550@zarco.pt>
2026-02-27 16:27:35 +0000

    Função para gerar um número aleatório e enviar para o aluno

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-radio.set_group(1)
-coluna = 0
-linha = 0
-alunoLed = 0
 numAleatorio = 0
+alunoLed = 0
+linha = 0
+coluna = 0
+radio.set_group(1)
 totalAlunos = 10
+
+def on_gesture_shake():
+    global numAleatorio
+    numAleatorio = randint(1, totalAlunos)
+    basic.show_number(numAleatorio)
+    radio.send_value("alunoAlt", numAleatorio)
+    basic.clear_screen()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
+input.onGesture(Gesture.Shake, function () {
+    numAleatorio = randint(1, totalAlunos)
+    basic.showNumber(numAleatorio)
+    radio.sendValue("alunoAlt", numAleatorio)
+    basic.clearScreen()
+})
 let numAleatorio = 0
-let alunoLed = 0
-let linha = 0
+let totalAlunos = 0
 let coluna = 0
+let linha = 0
+let alunoLed = 0
 radio.setGroup(1)
-let totalAlunos = 10
+totalAlunos = 10
