fb1605de57e1e3819456d1b51a72e28c41d695f8
Jorge Silva <jorgeneves550@zarco.pt>
2026-02-27 17:54:52 +0000

    Função para mudar o ícone do aluno em função do estado

---
+++ main.py
@@ -0,0 +1,9 @@
+handrise = 0
+radio.set_group(1)
+aluno = 1
+
+def intervir(num: number):
+    if handrise == 0:
+        basic.show_icon(IconNames.HAPPY)
+    else:
+        basic.show_icon(IconNames.CONFUSED)
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+function intervir (num: number) {
+    let handrise = 0
+    if (handrise == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Confused)
+    }
+}
+radio.setGroup(1)
+let aluno = 1
